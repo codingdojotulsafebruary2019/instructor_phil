146ac9ad838fe8e66ab3a23b55952daac1c6cc8f
Phil Krull <pkrull@codingdojo.com>
2019-03-27 14:20:04 -0500

    adding comments

--- Python/FlaskWithAjax/server.py
+++ Python/FlaskWithAjax/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, request
+# used to send json of user class
 import json
 from user import Cohort
 
 app = Flask(__name__)
 # get instance of cohort to simulate DB
-
 cohort = Cohort()
+
 @app.route("/")
 def index():
     return render_template("index.html", cohort = cohort)
@@ -13,6 +14,7 @@ def index():
 @app.route("/process", methods=["post"])
 def process():
     student = cohort.add(request.form["name"], request.form["location"])
+    # json.dumps() converts created student to json
     return json.dumps(student.__dict__)
 
 app.run(debug=True)
